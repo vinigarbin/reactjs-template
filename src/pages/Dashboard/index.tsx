6c1f1022d1ded7763c1ddd0790127e43482eb55e
Vinicius Garbin <vinigarbin@gmail.com>
2020-11-27 12:37:57 -0300

    primeiro commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => (
+  <Container>
+    <h1>Template</h1>
+  </Container>
+
+);
+
+export default Dashboard;
